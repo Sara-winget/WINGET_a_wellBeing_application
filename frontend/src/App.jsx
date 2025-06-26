fe27931021bd3771f33d562333e02a26053eefb9
sara <sarawinget16@gmail.com>
2025-06-26 09:56:32 +0530

    home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter  } from 'react-router-dom'
 import {Routes,Route}from 'react-router-dom'
 import LoginPage from './Pages/Login'
 import SignUpPage from './Pages/SignUp'
+import Home from './Pages/Home'
 function App() {
 
   
@@ -10,6 +11,7 @@ function App() {
     <>
       <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/SignUp' element={<SignUpPage/>}/>
       </Routes>
